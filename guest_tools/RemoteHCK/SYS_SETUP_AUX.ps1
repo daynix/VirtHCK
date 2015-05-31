c326f462a4e563c13552713d9401e391395cd710
Leonid Bloch <leonid@daynix.com>
2015-05-31 18:42:34 +0300

    VirtHCK: RemoteHCK: Changed control adapter naming
    This is to ease the identification of the message adapter by the HCK.
    Wiki for the manual setup updated accordingly.
    (see: https://github.com/daynix/VirtHCK/wiki#Checklist_for_a_new_client_VM)
    
    Signed-off-by: Leonid Bloch <leonid@daynix.com>

--- guest_tools/RemoteHCK/SYS_SETUP_AUX.ps1
+++ guest_tools/RemoteHCK/SYS_SETUP_AUX.ps1
@@ -9,7 +9,7 @@ if ( $NetAdd -ne $null )
 {
    $AdapterName = $NetAdd | Select-Object -ExpandProperty Name
    netsh interface ip set address name="$AdapterName" static 192.168.100.1 255.255.255.0;
-   $NetAdd | Rename-NetAdapter -NewName "Control_e1000";
+   $NetAdd | Rename-NetAdapter -NewName "Control";
    Set-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Value "C:\REPLACE-CONTROLLER-INST-FILE"
    Copy-Item "\\REPLACE-SMB-ADDRESS\qemu\REPLACE-CONTROLLER-INST-FILE" "C:\"
    Write-Host "Restarting in 30s"
@@ -22,7 +22,7 @@ if ( $NetAdd -ne $null )
 {
    $AdapterName = $NetAdd | Select-Object -ExpandProperty Name
    netsh interface ip set address name="$AdapterName" static 192.168.100.2 255.255.255.0;
-   $NetAdd | Rename-NetAdapter -NewName "Control_e1000";
+   $NetAdd | Rename-NetAdapter -NewName "MessageDevice";
    Set-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Value "C:\REPLACE-CLIENT-INST-FILE"
    Copy-Item "\\REPLACE-SMB-ADDRESS\qemu\REPLACE-CLIENT-INST-FILE" "C:\"
    Write-Host "Restarting in 30s"
@@ -35,7 +35,7 @@ if ( $NetAdd -ne $null )
 {
    $AdapterName = $NetAdd | Select-Object -ExpandProperty Name
    netsh interface ip set address name="$AdapterName" static 192.168.100.3 255.255.255.0;
-   $NetAdd | Rename-NetAdapter -NewName "Control_e1000";
+   $NetAdd | Rename-NetAdapter -NewName "MessageDevice";
    Set-Item -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce -Value "C:\REPLACE-CLIENT-INST-FILE"
    Copy-Item "\\REPLACE-SMB-ADDRESS\qemu\REPLACE-CLIENT-INST-FILE" "C:\"
    Write-Host "Restarting in 30s"
