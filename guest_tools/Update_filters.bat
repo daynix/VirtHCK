1e43769fd572fba32088ac22e4b15d05dca845c4
Leonid Bloch <leonid@daynix.com>
2015-08-10 16:37:27 +0300

    VirtHCK: Improve the standalone Update Kit Filters script
    The script now uses the DTMBIN environment variable as the path to the Controller installation location. This enables it to be shorter, clearer, and most importantly - more compatible with different installations, e.g. 32-bit ones.
    
    Signed-off-by: Leonid Bloch <leonid@daynix.com>

--- guest_tools/Update_filters.bat
+++ guest_tools/Update_filters.bat
@@ -1,35 +1,17 @@
 @echo off
 
+:: Filter updater for HCK and HLK
 :::::::::::::::::::::::::: Settings :::::::::::::::::::::::::::::::::
-
 :: Notice: As of July 2015, the HCK and the HLK filter updates are the exact same file, downloaded from the same location!
 SET "source=https://sysdev.microsoft.com/member/SubmissionWizard/LegalExemptions/HCKFilterUpdates.cab"
-:: version: 8 or 8.X for HCK, 10 or 10.X for HLK.
-SET version=10
 
 :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-SET midword=None
+SET "destination=C:\FilterUpdates.cab"
 
-if %version:~0,1%==8 (
-    SET midword=Certification
-)
-if %version:~0,2%==10 (
-    SET midword=Lab
-)
-if %midword%==None (
-    echo ERROR: only HCK and HLK ^(versions 8, 8.X, 10, 10.X^) are supported.
-    pause
-    exit /B 1
-)
-
-SET midletter=%midword:~0,1%
-SET "destination=C:\H%midletter%KFilterUpdates.cab"
-SET "KitDir=C:\Program Files (x86)\Windows Kits\%version%"
-
-if not exist "%KitDir%" (
-    echo ERROR: folder "%KitDir%"
-    echo does not exist! Please check that you specified the correct version.
+if not exist "%DTMBIN%\" (
+    echo ERROR: folder "%DTMBIN%"
+    echo does not exist! Please verify that you have the controller installed.
     pause
     exit /B 1
 )
@@ -37,15 +19,15 @@ if not exist "%KitDir%" (
 echo Please make sure that all instances of the Studio are turned OFF!
 pause
 
-echo Downloading H%midletter%K Filters...
-bitsadmin /transfer "Downloading H%midletter%K Filters" "%source%" "%destination%"
+echo Downloading Filters...
+bitsadmin /transfer "Downloading Filters" "%source%" "%destination%"
 if not errorlevel 0 echo ERROR & pause & exit /B 1
 echo Extracting...
-expand -i "%destination%" -f:UpdateFilters.sql "%KitDir%\Hardware %midword% Kit\Controller"
+expand -i "%destination%" -f:UpdateFilters.sql "%DTMBIN%\"
 if not errorlevel 0 echo ERROR & pause & exit /B 1
 echo Installing...
-pushd "%KitDir%\Hardware %midword% Kit\Controller"
+pushd "%DTMBIN%\"
 if not errorlevel 0 echo ERROR & pause & exit /B 1
-"%KitDir%\Hardware %midword% Kit\Controller\updatefilters.exe"
+"%DTMBIN%\updatefilters.exe"
 if not errorlevel 0 echo ERROR & pause & exit /B 1
 popd
