fb1b163dcb530f6e16dc88915659e78e44e7c58e
Leonid Bloch <leonid@daynix.com>
2016-01-11 18:53:33 +0200

    VirtHCK: KitAutosetup: Controller and Client installation scripts changed
    Now the Controller and the Client installation scripts (that are run
    automatically by SYS_SETUP) are checking for availability of the
    installation files, in order to reduce the chances of a silent failure.
    Also, some rewording and efficiency changes are made (do not reboot if
    the installation is not successful, etc.).
    
    Signed-off-by: Leonid Bloch <leonid@daynix.com>

--- guest_tools/KitAutosetup/CLIENT_INSTALL.bat
+++ guest_tools/KitAutosetup/CLIENT_INSTALL.bat
@@ -1,35 +1,34 @@
 @echo off
-echo Installing the HREPLACE-LETTERK Client. This will take a VERY long time!
-echo SERIOUSLY! IT WILL BE LONG!
-echo Grab yourself a meal!
-echo Waiting 30s...
+echo Waiting 30 s to stabilize...
 timeout /t 30 /nobreak > NUL
-echo Performing the installation - be VERY patient!
-cmd /C "\\HREPLACE-LETTERK-STUDIO\HREPLACE-LETTERKInstall\Client\setup.REPLACE-SUFFIX /qn ICFAGREE=Yes"
-
-if not errorlevel 0 (
-    echo Installation FAILED! Please install manually. & pause
-) else (
-    echo Installation SUCEEDED!
+set instfile="\\HREPLACE-LETTERK-STUDIO\HREPLACE-LETTERKInstall\Client\setup.REPLACE-SUFFIX"
+if not exist %instfile% (
+    echo The installation file does not exist in the specified path:
+    echo %instfile%
+    echo ^(Is the controller VM accessible?^) Please install manually!
+    pause & exit /B 1
 )
-
 echo The SMB share won't be avaliable on this machine after a shutdown cycle.
 echo If needed, it can be activated again by placing a file named "USE_SHARE" there and restarting via a shutdown.
-
 del "\\REPLACE-SMB-ADDRESS\qemu\USE_SHARE"
-
-echo ------
-echo Still, validating that SMB_SHARE is mapped to drive X:
-echo that may be needed if SMB_SHARE will be re-enabled...
-
+echo Still, validating that SMB_SHARE is mapped to drive X: ...
+echo ^(That may be needed if SMB_SHARE will be re-enabled^)
 if not exist x:\ (
     echo Was not mapped. Mapping...
-    net use X: \\REPLACE-SMB-ADDRESS\qemu /P:Yes
+    net use X: "\\REPLACE-SMB-ADDRESS\qemu" /P:Yes
 ) else (
     echo OK
 )
-echo ------
-
-echo Press ENTER to reboot the system...
-pause > NUL
-shutdown /t 10 /r /f
+echo Installing the HREPLACE-LETTERK Client. This will take a VERY long time!
+echo SERIOUSLY! IT WILL BE LONG!
+echo Grab yourself a meal!
+echo Performing the installation - be VERY patient!
+cmd /C %instfile% /qn ICFAGREE=Yes
+if not errorlevel 0 (
+    echo Installation FAILED! Please install manually.
+    pause & exit /B 1
+) else (
+    echo Installation FINISHED!
+)
+echo Rebooting in 30s...
+shutdown /t 30 /r /f
--- guest_tools/KitAutosetup/CONTROLLER_INSTALL.bat
+++ guest_tools/KitAutosetup/CONTROLLER_INSTALL.bat
@@ -1,29 +1,31 @@
 @echo off
-echo Installing HREPLACE-LETTERK Controller and Studio. This will take a VERY long time!
-echo SERIOUSLY! IT CAN TAKE OVER AN HOUR!
-echo Grab yourself a meal!
-echo Waiting 30s...
+echo Waiting 30 s to stabilize...
 timeout /t 30 /nobreak > NUL
-echo Performing the installation - be VERY patient!
-"\\REPLACE-SMB-ADDRESS\qemu\REPLACE-INSTALL-DIR\HREPLACE-LETTERKSetup.exe" /q
-
-if not errorlevel 0 (
-    echo Installation FAILED! Please install manually. & pause
-) else (
-    echo Installation SUCEEDED!
-    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "UpdateFilters" /t REG_SZ /d "C:\Users\Administrator\Desktop\UpdateFilters.bat" /f
+set instfile="\\REPLACE-SMB-ADDRESS\qemu\REPLACE-INSTALL-DIR\HREPLACE-LETTERKSetup.exe"
+if not exist %instfile% (
+    echo The installation file does not exist in the specified path:
+    echo %instfile%
+    echo Please install manually!
+    pause & exit /B 1
 )
-
-echo ------
 echo Validating that SMB_SHARE is mapped to drive X: ...
-
 if not exist x:\ (
     echo Was not mapped. Mapping...
-    net use X: \\REPLACE-SMB-ADDRESS\qemu /P:Yes
+    net use X: "\\REPLACE-SMB-ADDRESS\qemu" /P:Yes
 ) else (
     echo OK
 )
-echo ------
-
+echo Installing HREPLACE-LETTERK Controller and Studio. This will take a VERY long time!
+echo SERIOUSLY! IT CAN TAKE OVER AN HOUR!
+echo Grab yourself a meal!
+echo Performing the installation - be VERY patient!
+%instfile% /q
+if not errorlevel 0 (
+    echo Installation FAILED! Please install manually.
+    pause & exit /B 1
+) else (
+    echo Installation FINISHED!
+    reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce" /v "UpdateFilters" /t REG_SZ /d "C:\Users\Administrator\Desktop\UpdateFilters.bat" /f
+)
 echo Rebooting in 30s...
 shutdown /t 30 /r /f
